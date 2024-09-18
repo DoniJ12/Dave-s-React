73c77870c660235fd962c0d8c02939f5cd75ffa6
Doni J <donijosy67@gmail.com>
2024-09-18 09:27:45 +0300

    Practiced some click events

--- src/Content.js
+++ src/Content.js
@@ -4,8 +4,26 @@ const Content = () => {
   const names = ["Chaltu", "Abebe", "Kebede"];
   const ran = Math.floor(Math.random() * 3);
   const name = names[ran];
+
+  function handleClick(e) {
+    if (e.target.innerText === "Click") {
+      e.target.innerText = "Done!";
+    } else {
+      e.target.innerText = "Click";
+    }
+    // let text = e;
+    // console.log(text);
+    // text.target.innerText = "Abebe";
+  }
   return (
     <main>
+      <button
+        onClick={(e) => {
+          handleClick(e);
+        }}
+      >
+        Click
+      </button>
       <p>Hello {name}</p>
     </main>
   );
